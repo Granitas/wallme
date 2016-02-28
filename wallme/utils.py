e9e452e50d0357eca31079fd97624037399c6e4d
Bernard <bernardas@scrapinghub.com>
2016-02-28 23:55:35 +0200

    improve url_fix function to urljoin and handle https

--- wallme/utils.py
+++ wallme/utils.py
@@ -1,4 +1,10 @@
-def fix_url(url):
+from urllib.parse import urljoin
+
+
+def fix_url(url, parent=None):
+    http = 'https:' if 'https' in url else 'http:'
     if url.startswith('//'):
-        url = 'http:' + url
+        url = http + url
+    if parent and url.startswith('/'):
+        return urljoin(parent, url)
     return url
