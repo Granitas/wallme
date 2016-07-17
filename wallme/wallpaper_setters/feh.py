fc25d51b21968f033ec69f98a95a0cc268a51492
Bernardas <bernardas.alisauskas@gmail.com>
2016-07-17 15:49:49 +0000

    [setter/feh] call absolute path of feh (to work better with cron)

--- wallme/wallpaper_setters/feh.py
+++ wallme/wallpaper_setters/feh.py
@@ -1,5 +1,7 @@
 import subprocess
 
+ABS_FEH = subprocess.check_output(['which', 'feh']).decode('utf-8').strip()
+
 
 def set_wallpaper(image_loc):
-    subprocess.Popen('feh --bg-fill {}'.format(image_loc), shell=True)
+    subprocess.Popen('{} --bg-fill {}'.format(ABS_FEH, image_loc), shell=True)
