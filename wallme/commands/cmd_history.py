f4cee93375e7d2528dfafab5bbcf697875473d05
Bernardas <bernardas.alisauskas@gmail.com>
2016-07-18 01:19:15 +0000

    [commands/history] add position argument

--- wallme/commands/cmd_history.py
+++ wallme/commands/cmd_history.py
@@ -6,19 +6,22 @@ from wallme.history import get_history
 
 
 @click.command('history', help='display history')
-@click.option('-n', '--count', 'count', default=1, help='amount of items to display')
+@click.option('-c', '--count', 'count', default=1, help='amount of items to display')
+@click.option('-p', '--position', 'position', default=None, help='amount of items to display')
 @click.option('--year_month', help='filter specific month of the year, format %Y%m')
 @click.option('--raw', is_flag=True, help='return raw json for parsing')
-def cli(**kwargs):
-    count = kwargs['count']
-    year_month = kwargs['year_month']
-    raw = kwargs['raw']
-    items = list(get_history(year_month))
-    items = items[::-1][:count][::-1]
+def cli(count, year_month, raw, position, **kwargs):
+
+    def print_item(item):
+        click.echo(json.dumps(item, sort_keys=True, indent=2))
+
+    all_items = list(get_history(year_month))
+    if position is not None:
+        print_item(all_items[int(position)][0])
+        return
+    items = all_items[::-1][:count][::-1]
     if raw:
-        click.echo(json.dumps(items, sort_keys=True, indent=2))
+        print_item(items)
         return
     for item in items:
-        click.echo(json.dumps(item[0], sort_keys=True, indent=2))
-
-
+        print_item(item[0])
