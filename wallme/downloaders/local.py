da1d0bc79bc226727a75e7ab2cd326e514d9a6d2
Bernardas <bernardas.alisauskas@gmail.com>
2016-07-18 00:35:49 +0000

    [downloader/local] rework to use history.py module

--- wallme/downloaders/local.py
+++ wallme/downloaders/local.py
@@ -1,11 +1,11 @@
-import json
 import sys
 import os
 import random
 
 from requests.models import Response
-from wallme import settings
+
 from wallme.downloaders.base import BaseDownloader
+from wallme.history import get_history
 from wallme.image_downloaders.content import make_content
 
 
@@ -24,21 +24,8 @@ class LocalNonDownloader(BaseDownloader):
         :param position - position of image to ues, default random
         :return: dict{'content': <image_content>, <some meta data>...}
         """
-        history_loc = os.path.join(settings.WALLME_DIR, 'history')
-
-        def get_images(ym):
-            with open(os.path.join(history_loc, ym, 'history.json'), 'r') as f:
-                history_json = json.loads(f.read())
-            return [os.path.join(history_loc, ym, i['name']) for i in history_json['items']]
-
-        images = []
-        if not year_month:
-            yms = sorted(os.listdir(history_loc), key=int)
-            for ym in yms:
-                images.extend(get_images(ym))
-        else:
-            images = get_images(year_month)
-        if not position:
+        images = [os.path.join(loc, i['name']) for i, loc in get_history(year_month)]
+        if position is None:
             image = random.choice(images)
         elif position <= len(images) or position < 0:
             image = images[position]
